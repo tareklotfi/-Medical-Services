4d83fe144d6f1557217875875e6c5a49ea72cb1e
tareklotfi <62400248+tareklotfi@users.noreply.github.com>
2022-01-01 13:34:10 +0200

    MS-9
    Add login feature

--- main.dart
+++ main.dart
@@ -1,24 +1,105 @@
 import 'package:flutter/material.dart';
-
+ 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+ 
+class MyApp extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+ 
+class _State extends State<MyApp> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+ 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Medical services',
-      home: Scaffold(
+    return Scaffold(
         appBar: AppBar(
-          title: const Text('Welcome to Medical services'),
+          title: Text('Medical services'),
         ),
-        body: const Center(
-          child: Text('Hello '),
-        ),
-      ),
-    );
+        body: Padding(
+            padding: EdgeInsets.all(10),
+            child: ListView(
+              children: <Widget>[
+                Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      'Patient',
+                      style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 30),
+                    )),
+                Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      'Sign in',
+                      style: TextStyle(fontSize: 20),
+                    )),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    controller: nameController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'User Name',
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                  child: TextField(
+                    obscureText: true,
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                    ),
+                  ),
+                ),
+                FlatButton(
+                  onPressed: (){
+                    //forgot password screen
+                  },
+                  textColor: Colors.blue,
+                  child: Text('Forgot Password'),
+                ),
+                Container(
+                  height: 50,
+                    padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    child: RaisedButton(
+                      textColor: Colors.white,
+                      color: Colors.blue,
+                      child: Text('Login'),
+                      onPressed: () {
+                        print(nameController.text);
+                        print(passwordController.text);
+                      },
+                    )),
+                Container(
+                  child: Row(
+                    children: <Widget>[
+                      Text('Does not have account?'),
+                      FlatButton(
+                        textColor: Colors.blue,
+                        child: Text(
+                          'Sign in',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        onPressed: () {
+                          //signup screen
+                        },
+                      )
+                    ],
+                    mainAxisAlignment: MainAxisAlignment.center,
+                ))
+              ],
+            )));
   }
 }
